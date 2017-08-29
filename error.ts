29b07404684e2556059328d07952374cc906c872
Bruno Carvalho <a39591@alunos.isel.ipl.pt>
2017-08-29 21:03:29 +0100

    erroradd

--- error.ts
+++ error.ts
@@ -1,9 +1,10 @@
 export class Error {
+	
     public readonly errNum: number;
     public readonly message: string;
 
     constructor(num: number, message: string) {
-        this.errNum = num;
+			this.errNum = num;
         this.message = message;
     }
 }
